8ade62b5f788fa5acee01b20615d33b3987feb48
Anastasia Hladilka <nastiaklein2049@gmail.com>
2023-12-23 22:28:16 +0200

    add 2nd homework

---
+++ 2/script.js
@@ -0,0 +1,13 @@
+// the first part
+const userName = prompt("What is your name?");
+const userAge = prompt("How old are you?");
+const userLocation = prompt("What is your current location?");
+alert(
+  `Hello, ${userName}, I possess the information that you are ${userAge}, and this is your exact current location: ${userLocation}.`
+);
+// the second part
+const randomNumber = 53678;
+const randomString = String(randomNumber);
+alert(
+  `${randomString[2]} ${randomString[0]} ${randomString[3]} ${randomString[1]} ${randomString[4]}`
+);
