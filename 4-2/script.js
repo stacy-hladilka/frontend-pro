ab1f04c0f7b102578d536c5b7ecb00a12ee5ad56
Anastasia Hladilka <nastiaklein2049@gmail.com>
2024-01-06 16:12:47 +0200

    fix 4-2 homework

--- 4-2/script.js
+++ 4-2/script.js
@@ -1,64 +1,60 @@
-function askUserData() {
-  const userBirthYear = prompt("Enter your year of birth");
-  if (userBirthYear === null) {
-    alert("It's pity, you didn't enter your year of birth");
-    return;
-  }
-  const userBirthYearInt = parseInt(userBirthYear);
-  if (
-    isNaN(userBirthYearInt) ||
-    userBirthYearInt > 2024 ||
-    userBirthYearInt < 1900
-  ) {
-    alert("Invalid year of birth");
-    return;
-  }
-  const userLocation = prompt("Enter your location city");
-  if (userLocation === null) {
-    alert("It's pity, you didn't enter your location city");
-    return;
-  }
-  const userSport = prompt("What is your favorite kind of sport?");
-  if (userSport === null) {
-    alert("It's pity, you didn't enter your favorite kind of sport");
-    return;
-  }
+const userBirthYear = prompt("Enter your year of birth");
+const userLocation = prompt("Enter your location city");
+const userSport = prompt("What is your favorite kind of sport?");
+
+let birthMessage;
+const userBirthYearInt = parseInt(userBirthYear);
+if (userBirthYear === null) {
+  birthMessage = "It's pity, you didn't enter your year of birth.";
+} else if (
+  isNaN(userBirthYearInt) ||
+  userBirthYearInt > 2024 ||
+  userBirthYearInt < 1900
+) {
+  birthMessage = "Invalid year of birth.";
+} else {
   const currentYear = new Date().getFullYear();
-  const birthMessage = `Your age is ${currentYear - userBirthYearInt}.`;
-  let cityMessage;
-  switch (userLocation) {
-    case "Kyiv":
-      cityMessage = "You live in the capital of Ukraine!";
-      break;
-    case "Washington":
-      cityMessage = "You live in the capital of the US!";
-      break;
-    case "London":
-      cityMessage = "You live in the capital of the UK!";
-      break;
-    case "":
-      cityMessage = "You live in some empty space.";
-      break;
-    default:
-      cityMessage = `You live in ${userLocation}.`;
-  }
-  let sportMessage;
-  switch (userSport) {
-    case "Swimming":
-      sportMessage = "Do you want to be like Michael Phelps?";
-      break;
-    case "Tennis":
-      sportMessage = "Do you want to be like Elina Svitolina?";
-      break;
-    case "High Jump":
-      sportMessage = "Do you want to be like Yaroslava Mahuchikh?";
-      break;
-    case "":
-      sportMessage = "You enjoy emptiness.";
-      break;
-    default:
-      sportMessage = `${userSport} is a perfect choice!`;
-  }
-  alert(`${birthMessage} ${cityMessage} ${sportMessage}`);
+  birthMessage = `Your age is ${currentYear - userBirthYearInt}.`;
 }
-askUserData();
+
+let cityMessage;
+switch (userLocation) {
+  case null:
+    cityMessage = "It's pity, you didn't enter your location city.";
+    break;
+  case "Kyiv":
+    cityMessage = "You live in the capital of Ukraine!";
+    break;
+  case "Washington":
+    cityMessage = "You live in the capital of the US!";
+    break;
+  case "London":
+    cityMessage = "You live in the capital of the UK!";
+    break;
+  case "":
+    cityMessage = "You live in some empty space.";
+    break;
+  default:
+    cityMessage = `You live in ${userLocation}.`;
+}
+let sportMessage;
+switch (userSport) {
+  case null:
+    sportMessage = "It's pity, you didn't enter your favorite kind of sport.";
+    break;
+  case "Swimming":
+    sportMessage = "Do you want to be like Michael Phelps?";
+    break;
+  case "Tennis":
+    sportMessage = "Do you want to be like Elina Svitolina?";
+    break;
+  case "High Jump":
+    sportMessage = "Do you want to be like Yaroslava Mahuchikh?";
+    break;
+  case "":
+    sportMessage = "You enjoy emptiness.";
+    break;
+  default:
+    sportMessage = `${userSport} is a perfect choice!`;
+}
+alert(`${birthMessage} ${cityMessage} ${sportMessage}`);
